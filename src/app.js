19cdca9f912dd647b632bd8c0c1d33430cf2f509
leoescobarh <ldescobarherrera@gmail.com>
2022-06-11 22:53:54 +0000

    algo de avance :(

--- src/app.js
+++ src/app.js
@@ -6,6 +6,40 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger.com,", "racoon.com,"];
+
+  document.querySelector("p").innerHTML =
+    pronoun[0] +
+    adj[0] +
+    noun[0] +
+    " " +
+    pronoun[0] +
+    adj[0] +
+    noun[1] +
+    " " +
+    pronoun[1] +
+    adj[0] +
+    noun[0] +
+    " " +
+    pronoun[1] +
+    adj[0] +
+    noun[1] +
+    " " +
+    pronoun[0] +
+    adj[1] +
+    noun[0] +
+    " " +
+    pronoun[0] +
+    adj[1] +
+    noun[1] +
+    " " +
+    pronoun[1] +
+    adj[1] +
+    noun[0] +
+    " " +
+    pronoun[1] +
+    adj[1] +
+    noun[1];
 };
